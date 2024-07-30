0ec775fc3ba7cea6ccaf91bbd29e00e97bc566f5
maya88en <maya88en@gmail.com>
2024-07-30 18:45:29 +0300

    ESLint extention, improve search only if enter text, add loading

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { Form } from 'react-bootstrap';
 import './index.css';
 import { Button } from 'react-bootstrap';
@@ -12,27 +12,36 @@ const App = () => {
   const [images, setImages] = useState([]);
   const [totalPages, setTotalPages] = useState(0);
   const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const [error, setErrorMsg] = useState(false);
+
+
+  const fetchImages = useCallback(async () => {
+    try {
+      if (searchInput.current.value) {
+        setErrorMsg('');
+        setLoading(true);
+        const { data } = await axios.get(
+          `${API_URL}?query=${
+            searchInput.current.value
+          }&page=${page}&per_page=${IMAGES_PER_PAGE}&client_id=${
+            import.meta.env.VITE_API_KEY
+          }`
+        );
+        setImages(data.results);
+        setTotalPages(data.total_pages);
+        setLoading(false);
+      }
+    } catch (error) {
+      setErrorMsg('Error fetching images. Try again later.');
+      console.log(error);
+      setLoading(false);
+    }
+  }, [page]);
 
   useEffect(() => {
     fetchImages();
-  }, [page]);
-
-  const fetchImages = async () => {
-    try {
-      const { data } = await axios.get(
-        `${API_URL}?query=${
-          searchInput.current.value
-        }&page=${page}&per_page=${IMAGES_PER_PAGE}&client_id=${
-          import.meta.env.VITE_API_KEY
-        }`
-      );
-      console.log('data', data);
-      setImages(data.results);
-      setTotalPages(data.total_pages);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  }, [fetchImages]);
 
   const resetSearch = () => {
     setPage(1);
@@ -72,25 +81,30 @@ const App = () => {
         <div onClick={() => handleSelection('shoes')}>Shoes</div>
       </div>
 
-      <div className='images'>
-        {images.map((image) => (
-          <img
-            key={image.id}
-            src={image.urls.small}
-            alt={image.alt_description}
-            className='image'
-          />
-        ))}
-      </div>
-
-      <div className='buttons'>
-        {page > 1 && (
-          <Button onClick={() => setPage(page - 1)}>Previous</Button>
+        {loading ? (
+          <p className='loading'>Loading...</p>
+        ) : (
+          <>
+            <div className='images'>
+              {images.map((image) => (
+                <img
+                  key={image.id}
+                  src={image.urls.small}
+                  alt={image.alt_description}
+                  className='image'
+                />
+              ))}
+            </div>
+            <div className='buttons'>
+              {page > 1 && (
+                <Button onClick={() => setPage(page - 1)}>Previous</Button>
+              )}
+              {page < totalPages && (
+                <Button onClick={() => setPage(page + 1)}>Next</Button>
+              )}
+            </div>
+          </>
         )}
-        {page < totalPages && (
-          <Button onClick={() => setPage(page + 1)}>Next</Button>
-        )}
-      </div>
 
 
     </div>
